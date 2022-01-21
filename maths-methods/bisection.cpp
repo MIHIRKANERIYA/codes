cf38aa11e4be82e7b437e61b5a24c4936c1957a2
hidekiriyuga <82736639+hidekiriyuga@users.noreply.github.com>
2022-01-21 12:28:22 +0530

    Create bisection.cpp

---
+++ maths-methods/bisection.cpp
@@ -0,0 +1,35 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+#define EPSILON 0.01
+double func(double x)
+{
+	return x*x*x - x*x + 2;
+}
+
+void bisection(double a, double b)
+{
+	if (func(a) * func(b) >= 0)
+	{
+		cout << "You have not assume right\n";
+		return;
+	}
+
+	double c = a;
+	
+		c = (a+b)/2;
+		if (func(c) == 0.0)
+			cout<<c;
+		else if (func(c)*func(a) < 0)
+			bisection(a,c);
+		else
+			bisection(c,b);
+	
+	
+}
+int main()
+{
+	double a =-200, b = 300;
+	bisection(a, b);
+	return 0;
+}
